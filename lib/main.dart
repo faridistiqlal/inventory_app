6d9ad792bea39d6b5da2ef486cd2c6e624d83e03
Farid Istiqlal <farid.istiqlal@outlook.com>
2022-03-30 09:41:23 +0700

    provider darktheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,30 @@
+// import 'package:flutter/material.dart';
+// import 'package:inventory_app/view/splashscreen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Inventory App',
+//       theme: ThemeData.light(),
+//       home: const SplashScreen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:inventory_app/view/splashscreen.dart';
+import 'package:provider/provider.dart';
+
+import 'style/theme_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +33,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inventory App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+        builder: (context, ThemeNotifier notifier, child) {
+          return MaterialApp(
+            title: "Flutter Provider",
+            debugShowCheckedModeBanner: false,
+            theme: notifier.darkTheme ? dark : light,
+            home: const SplashScreen(),
+          );
+        },
       ),
-      home: const SplashScreen(),
     );
   }
 }
