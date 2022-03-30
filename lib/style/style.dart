d47fa8186a1b53646ca866e53a60caa91f4ba8e0
Farid Istiqlal <farid.istiqlal@outlook.com>
2022-03-30 09:42:08 +0700

    add style

---
+++ lib/style/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(255, 248, 161, 30);
+const bgCOlor = Colors.white;
+const iconColor = Colors.black;
+const subtitleText = Colors.black54;
+const titleText = Colors.black;
+const defaultPadding = 10.0;
