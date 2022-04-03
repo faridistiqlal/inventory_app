3240c1be3ee457cfd05011c508f94c2303430ff0
Farid Istiqlal <farid.istiqlal@outlook.com>
2022-04-03 16:01:16 +0700

    disable text login in header

--- lib/class/header_login.dart
+++ lib/class/header_login.dart
@@ -19,7 +19,7 @@ class HeaderLogin extends StatelessWidget {
           builder: (context, notifier, child) => notifier.darkTheme
               ? Container(
                   width: mediaQueryData.size.width,
-                  height: mediaQueryData.size.height * 0.18,
+                  height: mediaQueryData.size.height * 0.2,
                   decoration: const BoxDecoration(
                     color: Color.fromARGB(255, 34, 33, 33),
                     // borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -27,31 +27,31 @@ class HeaderLogin extends StatelessWidget {
                 )
               : Container(
                   width: mediaQueryData.size.width,
-                  height: mediaQueryData.size.height * 0.18,
+                  height: mediaQueryData.size.height * 0.2,
                   decoration: const BoxDecoration(
                     color: Colors.amber,
                     // borderRadius: BorderRadius.all(Radius.circular(20.0)),
                   ),
                 ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: defaultPadding * 4,
-            horizontal: defaultPadding * 2,
-          ),
-          child: Text(
-            "Login User",
-            style: Theme.of(context).textTheme.headline5?.copyWith(
-                // color: ,
-                fontWeight: FontWeight.w700,
-                fontSize: 30),
-          ),
-        ),
+        // Padding(
+        //   padding: const EdgeInsets.symmetric(
+        //     vertical: defaultPadding * 4,
+        //     horizontal: defaultPadding * 2,
+        //   ),
+        //   child: Text(
+        //     "Login User",
+        //     style: Theme.of(context).textTheme.headline5?.copyWith(
+        //         // color: ,
+        //         fontWeight: FontWeight.w700,
+        //         fontSize: 30),
+        //   ),
+        // ),
         Positioned(
           top: mediaQueryData.size.height * 0.01,
           left: mediaQueryData.size.height * 0.03,
           child: Image.asset(
-            'assets/logo/person1.png',
+            'assets/logo/person2.png',
             width: mediaQueryData.size.height * 0.7,
             height: mediaQueryData.size.width * 0.7,
           ),
