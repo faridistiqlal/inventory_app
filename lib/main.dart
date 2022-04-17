bbf1b68e0ed38cc40fe5657f8997e11d0b2748de
Farid Istiqlal <farid.istiqlal@outlook.com>
2022-04-17 22:31:57 +0700

    add navigator page

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,36 @@
-// import 'package:flutter/material.dart';
-// import 'package:inventory_app/view/splashscreen.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Inventory App',
-//       theme: ThemeData.light(),
-//       home: const SplashScreen(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:inventory_app/function/add_sparepart.dart';
+import 'package:inventory_app/tab/tab_admin.dart';
+import 'package:inventory_app/view/admin/hal_admin.dart';
+import 'package:inventory_app/view/admin/hal_profile_admin.dart';
+import 'package:inventory_app/view/admin/menuadmin/hal_sparepart.dart';
+import 'package:inventory_app/view/login_page.dart';
 import 'package:inventory_app/view/splashscreen.dart';
-import 'package:provider/provider.dart';
-
-import 'style/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeNotifier(),
-      child: Consumer<ThemeNotifier>(
-        builder: (context, ThemeNotifier notifier, child) {
-          return MaterialApp(
-            title: "Flutter Provider",
-            debugShowCheckedModeBanner: false,
-            theme: notifier.darkTheme ? dark : light,
-            home: const SplashScreen(),
-          );
-        },
-      ),
-    );
-  }
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: const Color.fromARGB(255, 248, 161, 30),
+      colorScheme: const ColorScheme.light()
+          .copyWith(primary: const Color.fromARGB(255, 248, 161, 30)),
+    ),
+    debugShowCheckedModeBanner: false,
+    title: 'Inventory App',
+    routes: <String, WidgetBuilder>{
+      '/HalAdmin': (BuildContext context) => const HalAdmin(),
+      '/LoginPage': (BuildContext context) => const LoginPage(),
+      '/HalTabAdmin': (BuildContext context) => const HalTabAdmin(),
+      '/HalProfilAdmin': (BuildContext context) => const HalProfilAdmin(),
+      '/HalSparepart': (BuildContext context) => const HalSparepart(),
+      '/AddSparepart': (BuildContext context) => const AddSparepart(),
+    },
+    home: const SplashScreen(),
+  ));
 }
