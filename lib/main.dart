49ce42a00115d8efcc413291b3c6b74e5ba07c9a
Farid Istiqlal <farid.istiqlal@outlook.com>
2022-04-21 08:58:06 +0700

    add navigator

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:inventory_app/function/add_sparepart.dart';
 import 'package:inventory_app/tab/tab_admin.dart';
+import 'package:inventory_app/view/admin/detail/detail_sparepart.dart';
 import 'package:inventory_app/view/admin/hal_admin.dart';
 import 'package:inventory_app/view/admin/hal_profile_admin.dart';
 import 'package:inventory_app/view/admin/menuadmin/hal_sparepart.dart';
 import 'package:inventory_app/view/login_page.dart';
+import 'package:inventory_app/view/mekanik/hal_mekanik.dart';
+import 'package:inventory_app/view/mekanik/input/post_request_sparepart.dart';
+import 'package:inventory_app/view/pimpinan/hal_pimpinna.dart';
 import 'package:inventory_app/view/splashscreen.dart';
 
 void main() {
@@ -30,6 +34,13 @@ void main() {
       '/HalProfilAdmin': (BuildContext context) => const HalProfilAdmin(),
       '/HalSparepart': (BuildContext context) => const HalSparepart(),
       '/AddSparepart': (BuildContext context) => const AddSparepart(),
+      '/HalMekanik': (BuildContext context) => const HalMekanik(),
+      '/HalMesinMekanik': (BuildContext context) => const HalMesinMekanik(),
+      '/PostRequestSparepart': (BuildContext context) =>
+          const PostRequestSparepart(),
+      '/DetailSparepart': (BuildContext context) => const DetailSparepart(
+            dId: '',
+          ),
     },
     home: const SplashScreen(),
   ));
